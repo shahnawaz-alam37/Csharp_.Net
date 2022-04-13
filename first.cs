0f68a7e7449adb222bb04753f97bca3c18ec329f
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-04-13 12:18:16 -0700

    Exception handling

---
+++ first.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace program{
+    class data
+    {
+        int a=10,b=0;
+        public void sum(){
+            try
+            {
+                a=a/b;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("exception caugth:{0}",e);
+            }
+        }
+    }
+      class first{
+        public static void Main(string[]args){
+            data obj=new data();
+            obj.sum();
+            Console.ReadLine();
+        }
+    }
+}
