cd92996c95741cc9e64922578af016ddef96b3ca
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-04-14 10:59:03 -0700

    simple delegate

--- delegate.cs
+++ delegate.cs
@@ -1,9 +1,9 @@
 using System;
-public delegate void del_example(string mess);
+public delegate void del_example(string mess);//declaring delegate
 class pro{
     public static void Main(string[]args){
-        del_example del = new del_example(disp);
-        del("hello world");
+        del_example del = new del_example(disp);//passing method name as parameter
+        del("hello world");//calling method with parameter
     }
     public static void disp(string mess){
         Console.WriteLine(mess);
