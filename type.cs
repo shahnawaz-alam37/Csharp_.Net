743b9d89ff4aa851f37a0dc610c9836a37d16028
shahnawaz-alam37 <shahnawazalam.cme@gmail.com>
2022-04-15 08:38:35 -0700

    using the typeof operator

---
+++ type.cs
@@ -0,0 +1,10 @@
+using System;
+namespace typeof_operator{
+    class operator_type{
+        public static void Main(string[]args){
+            Console.WriteLine(sizeof(int));
+            Console.WriteLine(typeof(int));
+            Console.ReadLine();
+        }
+    }
+}
